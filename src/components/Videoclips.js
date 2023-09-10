a5a0cb0016c9eff0599e95c5e7fd9c5431aaa20d
harshil-063 <hk.bitcoding@gmail.com>
2023-09-10 22:40:46 +0530

    -- change download button color

--- src/components/Videoclips.js
+++ src/components/Videoclips.js
@@ -65,7 +65,7 @@ const Videoclips = ({ videoClips, setVideoCount }) => {
     {/* Download button */}
     {selectedRows.length > 0 && (
       <button
-        className="fixed bottom-0 right-0 m-4 p-4 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 z-50"
+        className="fixed bottom-0 right-0 m-4 p-4 bg-green-500 text-white rounded-full shadow-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 z-50"
         onClick={handleDownloadClick}
       ><LuDownload />
       </button>
