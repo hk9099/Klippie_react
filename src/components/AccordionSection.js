1ab6ca498c57e4531545cbc6f1f90d1c9a727269
hk9099 <khokhaniharsh0670@gmail.com>
2023-11-18 21:02:47 +0530

    feat : set download functinality

--- src/components/AccordionSection.js
+++ src/components/AccordionSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     Accordion,
     AccordionBody,
@@ -9,18 +9,23 @@ import { FiChevronDown, FiChevronRight } from "react-icons/fi";
 import Mainvideo from "./Mainvideo";
 import Videoclips from "./Videoclips";
 import { AiFillDelete } from "react-icons/ai";
+import { HiDownload } from "react-icons/hi";
 import { useFileSelected } from "../context/SelectionContext.js";
 import axios from "axios";
 import qs from "qs";
 import { TokenManager } from '../components/getToken.js';
 import { useSnackbar } from 'notistack';
+import JSZip from 'jszip';
 
 export default function AccordionSection({ videoClips, videoURl, clips }) {
-    const { fileselected, fileselecteddata,setFileDelete } = useFileSelected();
+    const { fileselected, fileselecteddata, setFileDelete } = useFileSelected();
     console.log(fileselecteddata, "fileselecteddata");
     const { enqueueSnackbar } = useSnackbar();
     const [openStates, setOpenStates] = useState([true, true]);
     const [videoCount, setVideoCount] = useState(videoClips.length);
+    const [downloadProgress, setDownloadProgress] = useState(0);
+    const [currentDownloadingVideo, setCurrentDownloadingVideo] = useState(null);
+    const [isDownloadModalOpen, setDownloadModalOpen] = useState(false);
     const userToken = TokenManager.getToken()[1]
     const toggleAccordion = (index) => {
         setOpenStates((prev) =>
@@ -28,7 +33,7 @@ export default function AccordionSection({ videoClips, videoURl, clips }) {
         );
     };
 
-    const handleAddClipClick = (event) => {
+    const handleDeleteClick = (event) => {
         event.stopPropagation();
         for (let i = 0; i < fileselecteddata.length; i++) {
             const element = fileselecteddata[i];
@@ -55,16 +60,109 @@ export default function AccordionSection({ videoClips, videoURl, clips }) {
                     console.log(JSON.stringify(response.data));
                     setVideoCount(videoCount - 1);
                     setFileDelete(true);
-                    enqueueSnackbar('Video deleted successfully', { variant: 'success',autoHideDuration: 1500, });
+                    enqueueSnackbar('Video deleted successfully', { variant: 'success', autoHideDuration: 1500, });
                 })
                 .catch((error) => {
                     console.log(error);
                     enqueueSnackbar(error.response.data.message,
-                     { variant: 'error' ,autoHideDuration: 1500,
-                    });
+                        {
+                            variant: 'error', autoHideDuration: 1500,
+                        });
                     setFileDelete(false);
                 });
 
+        }
+    };
+
+    useEffect(() => {
+        // Reset progress when fileselecteddata changes
+        setDownloadProgress(0);
+    }, [fileselecteddata]);
+
+    const handleDownloadClick = async (event) => {
+        event.stopPropagation();
+        try {
+            setDownloadModalOpen(true); // Open the modal
+
+            if (fileselecteddata.length === 1) {
+                // Download a single video
+                const element = fileselecteddata[0];
+                setCurrentDownloadingVideo(element.title);
+
+                const response = await axios({
+                    method: 'get',
+                    url: element.src,
+                    responseType: 'blob',
+                    onDownloadProgress: (progressEvent) => {
+                        const progress = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                        setDownloadProgress(progress);
+                    },
+                });
+
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', `${element.title}.${element.type}`);
+                document.body.appendChild(link);
+                link.click();
+                link.parentNode.removeChild(link);
+
+                enqueueSnackbar(`Downloaded: ${element.title}`, { variant: 'success' });
+            }else {
+                // Download a zip file containing all videos
+                const zip = new JSZip();
+    
+                const totalFiles = fileselecteddata.length;
+    
+                const handleFileDownload = async (element, index) => {
+                    setCurrentDownloadingVideo(element.title);
+    
+                    const response = await axios({
+                        method: 'get',
+                        url: element.src,
+                        responseType: 'blob',
+                    });
+    
+                    zip.file(`${element.title}.${element.type}`, response.data);
+    
+                    // Calculate progress based on the number of completed files
+                    const progress = Math.round(((index + 1) / totalFiles) * 100);
+                    setDownloadProgress(progress);
+    
+                    if (index + 1 === totalFiles) {
+                        // All files have been downloaded, generate and initiate zip file download
+                        const content = await zip.generateAsync({ type: "blob" });
+                        const zipFileName = "videos.zip";
+                        const zipFileUrl = window.URL.createObjectURL(content);
+    
+                        const zipLink = document.createElement('a');
+                        zipLink.href = zipFileUrl;
+                        zipLink.setAttribute('download', zipFileName);
+                        document.body.appendChild(zipLink);
+                        zipLink.click();
+                        zipLink.parentNode.removeChild(zipLink);
+    
+                        enqueueSnackbar(`Downloaded: ${zipFileName}`, { variant: 'success' });
+    
+                        // Reset progress and close the modal
+                        setDownloadProgress(0);
+                        setCurrentDownloadingVideo(null);
+                        setDownloadModalOpen(false);
+                    }
+                };
+    
+                // Initiate downloads for each file
+                for (let i = 0; i < fileselecteddata.length; i++) {
+                    await handleFileDownload(fileselecteddata[i], i);
+                }
+            }
+        } catch (error) {
+            console.error("Download Error:", error);
+            enqueueSnackbar("Download failed", { variant: 'error' });
+        } finally {
+            setDownloadProgress(0);
+            setCurrentDownloadingVideo(null);
+            setDownloadModalOpen(false); // Close the modal
         }
     };
 
@@ -117,12 +215,35 @@ export default function AccordionSection({ videoClips, videoURl, clips }) {
                             </div>
                         </AccordionHeader>
                         {fileselected && (
-                            <div className="flex items-center justify-between bg-[#FEE2E2] rounded-[10px] border border-red-500 p-2 cursor-pointer select-none" onClick={handleAddClipClick}>
-                                <span className="text-sm font-bold text-gray-900 mr-1">
-                                    Delete
-                                    ({fileselecteddata.length})
-                                </span>
-                                <AiFillDelete className="text-red-600 text-xl " />
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center justify-between bg-[#D1FAE5] rounded-[10px] border border-green-500 p-2 cursor-pointer select-none mr-2"
+                                    onClick={handleDownloadClick}>
+                                    <span className="text-sm font-bold text-gray-900 mr-1">
+                                        Download
+                                        ({fileselecteddata.length})
+                                    </span>
+                                    <HiDownload className="text-green-600 text-xl " />
+                                </div>
+                                <div className="flex items-center justify-between bg-[#FEE2E2] rounded-[10px] border border-red-500 p-2 cursor-pointer select-none" onClick={handleDeleteClick}>
+                                    <span className="text-sm font-bold text-gray-900 mr-1">
+                                        Delete
+                                        ({fileselecteddata.length})
+                                    </span>
+                                    <AiFillDelete className="text-red-600 text-xl " />
+                                </div>
+                            </div>
+                        )}
+                        {downloadProgress > 0 && isDownloadModalOpen && (
+                            <div className="fixed top-0 left-0 bottom-0 right-0 flex items-center justify-center  z-50 inset-0 backdrop-blur-3xl bg-black bg-opacity-60">
+                                <div className="bg-white p-4 rounded">
+                                    <h2 className="text-xl font-bold mb-2">{`Downloading ${currentDownloadingVideo}`}</h2>
+                                    <div className="bg-green-300 h-4 rounded-full mt-2">
+                                        <div
+                                            className="bg-green-600 h-full rounded-full"
+                                            style={{ width: `${downloadProgress}%` }}
+                                        />
+                                    </div>
+                                </div>
                             </div>
                         )}
                     </div>
